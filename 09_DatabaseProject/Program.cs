178c2912cddb36eec1a8ede7f3613e673071f5a2
Yasin <bmyasinozcelik@outlook.com>
2025-03-05 02:09:11 +0300

    New studies added - 09_Db

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Ado.net
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string selectNumber;
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("1- Kategoriler");
+            Console.WriteLine("2- Ürünler");
+            Console.WriteLine("3- Siparişler");
+            Console.WriteLine("4- Çıkış Yap");
+            Console.WriteLine("Lütfen getirmek istediğiniz tablo numarasını girin: ");
+            Console.WriteLine("--------------------------------------------------");
+            selectNumber = Console.ReadLine();
+
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-U383BCO;Initial Catalog=EgitimKampiDb;Integrated Security=True");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * from TblCategory", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            foreach(DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+
+            connection.Close();
+
+
+
+
+        }
+    }
+}
